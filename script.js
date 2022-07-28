d5ed9cbaaf8694107f5b6e459febf8294982ebe7
Marco <m_uno_p@hotmail.com>
2022-07-28 17:09:40 -0500

    Add buttons for changing read status and for deleting a selected book

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 class Book {
-  constructor(author, title, publicationYear) {
+  constructor(author, title, publicationYear, readStatus) {
     this.author = author;
     this.title = title;
     this.publicationYear = publicationYear;
+    this.readStatus = readStatus;
   }
 }
 
 let library = [];
-const form = document.querySelector('form');
+let form = document.querySelector('form');
 let inputAuthor = document.getElementById('author');
 let inputTitle = document.getElementById('title');
 let inputPublicationYear = document.getElementById('publicationYear');
+
 const tbody = document.querySelector('tbody');
 
 form.addEventListener('submit', (e) => {
@@ -19,8 +21,10 @@ form.addEventListener('submit', (e) => {
   let author = inputAuthor.value;
   let title = inputTitle.value;
   let publicationYear = inputPublicationYear.value;
+  let inputReadStatus = document.querySelector('input[name="readStatus"]:checked');
+  let readStatus = inputReadStatus.value;
 
-  let book = new Book(author, title, publicationYear);
+  let book = new Book(author, title, publicationYear, readStatus);
 
   inputAuthor.value = '';
   inputTitle.value = '';
@@ -32,11 +36,43 @@ form.addEventListener('submit', (e) => {
   let authorTr = document.createElement('td');
   let titleTr = document.createElement('td');
   let publicationYearTr = document.createElement('td');
+  let readStatusTr = document.createElement('td');
+  let changeReadStatusCell = document.createElement('td');
+  let changeReadStatusButton = document.createElement('button');
+  let deleteCell = document.createElement('td');
+  let deleteButton = document.createElement('button');
+
+  changeReadStatusCell.appendChild(changeReadStatusButton);
+  deleteCell.appendChild(deleteButton);
+
   authorTr.textContent = book.author;
   titleTr.textContent = book.title;
   publicationYearTr.textContent = book.publicationYear;
+  readStatusTr.textContent = readStatus;
+  
+  changeReadStatusButton.textContent = 'Change Read Status';
+  changeReadStatusButton.style.margin = 'auto';
+  deleteButton.textContent = 'Delete';
+  deleteButton.style.margin = 'auto';
+
   tableTr.appendChild(authorTr);
   tableTr.appendChild(titleTr);
   tableTr.appendChild(publicationYearTr);
+  tableTr.appendChild(readStatusTr);
+  tableTr.appendChild(changeReadStatusCell);
+  tableTr.appendChild(deleteCell);
+
   tbody.appendChild(tableTr);
+
+  changeReadStatusButton.addEventListener('click', () => {
+    if (readStatusTr.textContent == 'Yes') {
+      readStatusTr.innerText = 'No';
+    } else {
+      readStatusTr.innerText = 'Yes';
+    }
+  });
+
+  deleteButton.addEventListener('click', () => {
+    tbody.removeChild(tableTr);
+  });
 });
